509f679a9cf4b3747b242ea1b4af6a6d84b159f8
Matthew Lewis <matt@mplewis.com>
2013-12-29 18:25:52 -0600

    Add an exec_python hook

--- config.js
+++ config.js
@@ -18,6 +18,25 @@ var hooks = {
         res.send(400, {'error': 'You didn\'t give me anything to fry'});
       }
     }
+  },
+  exec_python: {
+    key: 'rng_key',
+    action: function(req, res) {
+      var range = req.query.range;
+      var to_exec = 'python -c "from random import randrange; print(randrange(100))"';
+      require('child_process').exec(to_exec, function(error, stdout, stderr) {
+        var retval = {};
+        retval.stdout = stdout;
+        retval.stderr = stderr;
+        if (error === null) {
+          retval.success = true;
+          res.send(200, retval);
+        } else {
+          retval.error = error;
+          res.send(500, retval);
+        }
+      });
+    }
   }
 };
 
