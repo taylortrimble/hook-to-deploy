cb48167ad3b0bee55e47373fdc666d6bcb6e9f5d
Matthew Lewis <matt@mplewis.com>
2013-12-29 18:57:03 -0600

    Add test for incorrect keys

--- test/test_server.js
+++ test/test_server.js
@@ -11,11 +11,19 @@ var client = restify.createJsonClient({
   url: 'http://localhost:8080'
 });
 
-describe('basic functionality', function() {
-  it('should send a 200 OK response', function(done) {
+describe('key correctness:', function() {
+  it('should send a 200 OK response for correct keys', function(done) {
     client.get('/hook/project_one?key=p1_key', function(err, req, res, data) {
       should.not.exist(err);
       res.statusCode.should.eql(200);
+      done();
+    });
+  });
+  it('should send a 403 Forbidden server error for incorrect keys', function(done) {
+    client.get('/hook/project_one?key=BAD_KEY', function(err, req, res, data) {
+      should.exist(err);
+      res.statusCode.should.eql(403);
+      data.error.code.should.eql('INCORRECT_KEY');
       done();
     });
   });
