d9e6da5b06e79c3ba4ccd736b189cf5b2839a5c4
Matthew Lewis <matt@mplewis.com>
2014-02-12 17:51:33 -0600

    Break out in-progress data validation

--- test/test_tasks.js
+++ test/test_tasks.js
@@ -5,17 +5,22 @@ var tasks = require('../tasks.js');
 
 var fs = require('fs');
 
+var procDataKeys = ['toExec', 'resultsFile', 'startedAt', 'inProgress'];
+
+function validateInProgressData(data) {
+  data.should.have.keys(procDataKeys);
+  data.toExec.should.equal('someFunction');
+  data.resultsFile.should.endWith('.json');
+  Date.parse(data.startedAt).should.be.ok;  // jshint ignore:line
+  data.inProgress.should.be.true;  // jshint ignore:line
+}
+
 describe('execSaveResults', function() {
   it('should fire a callback with in-progress data', function(done) {
     var stubFsWriteFile = sinon.stub(fs, 'writeFile').callsArg(2);
     tasks.execSaveResults('someFunction', 'resultsFolder', function(err, data) {
       should.not.exist(err);
-      data.should.have.keys('toExec', 'resultsFile', 'startedAt', 'inProgress');
-      data.toExec.should.equal('someFunction');
-      data.resultsFile.should.endWith('.json');
-      Date.parse(data.startedAt).should.be.ok;  // jshint ignore:line
-      data.inProgress.should.be.true;  // jshint ignore:line
-
+      validateInProgressData(data);
       stubFsWriteFile.restore();
       done();
     });
