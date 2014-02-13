7b66dcd8a40e3cc5f6eac53f164aafdbd2727af5
Matthew Lewis <matt@mplewis.com>
2014-02-12 19:04:26 -0600

    Add tests for saving in-progress and saving final data

--- test/test_tasks.js
+++ test/test_tasks.js
@@ -4,6 +4,7 @@ var sinon = require('sinon');
 var tasks = require('../tasks.js');
 
 var fs = require('fs');
+var child_process = require('child_process');
 
 var dataKeys = {
   inProgress: ['toExec', 'resultsFile', 'startedAt', 'inProgress'],
@@ -19,6 +20,22 @@ function validateInProgressData(data) {
   data.inProgress.should.be.true;  // jshint ignore:line
 }
 
+function validateFinishedData(data, failureFlag) {
+  if (failureFlag)
+    data.should.have.keys(dataKeys.failure);
+  else {
+    data.should.have.keys(dataKeys.success);
+    data.success.should.be.true;  //jshint ignore:line
+  }
+  data.toExec.should.equal('someFunction');
+  data.resultsFile.should.endWith('.json');
+  Date.parse(data.startedAt).should.be.ok;  // jshint ignore:line
+  Date.parse(data.finishedAt).should.be.ok;  // jshint ignore:line
+  data.stdout.should.eql(['Hello world', '']);
+  data.stderr.should.eql(['OK', '']);
+  data.inProgress.should.be.false;  // jshint ignore:line
+}
+
 describe('execSaveResults', function() {
   it('should fire a callback with in-progress data', function(done) {
     var stubFsWriteFile = sinon.stub(fs, 'writeFile').callsArg(2);
@@ -30,6 +47,46 @@ describe('execSaveResults', function() {
       if (stubFsWriteFile.callCount >= 2) {
         clearInterval(callChecker);
         stubFsWriteFile.restore();
+        done();
+      }
+    }, 1);
+  });
+  it('should save its in-progress data into the results file before the process starts', function(done) {
+    var stubFsWriteFile = sinon.stub(fs, 'writeFile').callsArg(2);
+    tasks.execSaveResults('someFunction', 'resultsFolder', function(err) {
+      should.not.exist(err);
+    });
+    var callChecker = setInterval(function() {
+      if (stubFsWriteFile.callCount >= 2) {
+        clearInterval(callChecker);
+        
+        var jsonData = stubFsWriteFile.getCall(0).args[1];
+        var parsedData = JSON.parse(jsonData);
+        validateInProgressData(parsedData);
+
+        stubFsWriteFile.restore();
+        done();
+      }
+    }, 1);
+  });
+  it('should save its final data into the results file after the process ends', function(done) {
+    var stdout = 'Hello world\n';
+    var stderr = 'OK\n';
+    var stubFsWriteFile = sinon.stub(fs, 'writeFile').callsArg(2);
+    var stubExec = sinon.stub(child_process, 'exec').callsArgWith(1, null, stdout, stderr);
+    tasks.execSaveResults('someFunction', 'resultsFolder', function(err) {
+      should.not.exist(err);
+    });
+    var callChecker = setInterval(function() {
+      if (stubFsWriteFile.callCount >= 2) {
+        clearInterval(callChecker);
+        
+        var jsonData = stubFsWriteFile.getCall(1).args[1];
+        var parsedData = JSON.parse(jsonData);
+        validateFinishedData(parsedData, false);
+
+        stubFsWriteFile.restore();
+        stubExec.restore();
         done();
       }
     }, 1);
